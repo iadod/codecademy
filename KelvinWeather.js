63fcbf7daa406f2b4bda0bc4836ba996cbbab1d6
iadod <84059426+iadod@users.noreply.github.com>
2021-07-10 07:24:07 -0600

    Update and rename Java Script to KelvinWeather.js
    script to translate kelvin to celsius and newton

---
+++ KelvinWeather.js
@@ -0,0 +1,14 @@
+
+const kelvin = 0;
+/set constant variable kelvin to 293 /
+console.log(kelvin)
+const celsius = kelvin - 273;
+/set constant variable celsius to kelvin - 273/
+let fahrenheit = celsius * (9 / 5) + 32;
+/set let var fahrenheit to conversion fomula/
+fahrenheit = Math.floor(fahrenheit);
+/rounds fahrenheit value down/
+console.log(`The temperature is ${fahrenheit} degrees Fahrenheit.`);
+let newton = celsius * (33 / 100);
+newton = Math.floor(newton);
+console.log(`temp is ${newton} newton`);
