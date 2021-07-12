d8c1b3df2a8c47467284a3993660ad287dd9b86f
iadod <84059426+iadod@users.noreply.github.com>
2021-07-12 10:13:36 -0600

    Create elseIf.js

---
+++ elseIf.js
@@ -0,0 +1,18 @@
+let season = 'spring';
+
+if (season === 'spring') {
+  console.log('It\'s spring! The trees are budding!');
+} 
+else if (season === 'winter') {console.log ('It\'s winter! Everything is covered in snow.')
+} 
+else if (season === 'fall') {console.log ('It\'s fall! Leaves are falling!')
+} 
+else if (season === 'summer') {console.log ('It\'s sunny and warm because it\'s summer!')
+} 
+else {
+  console.log('Invalid season.');
+}
+
+
+
+//the outcome of the variable season determines which of the four else if statemants prints./
