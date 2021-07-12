178196cbc898b25878d4ea6db279334ab5ae99cd
iadod <84059426+iadod@users.noreply.github.com>
2021-07-11 19:02:49 -0600

    Create dogYears.js

---
+++ dogYears.js
@@ -0,0 +1,17 @@
+const myAge = 26;
+/ const variable my age set to 26/
+let earlyYears = 2;
+/variable earlyYears is set to 2/
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+/new variable that uses math on an existing variable/
+laterYears *= 4;
+/use multiplication assignment operator to multiply laterYears by 4/
+console.log(earlyYears);
+console.log(laterYears);
+let myAgeInDogYears = earlyYears + laterYears
+console.log(myAgeInDogYears);
+let myName = 'Grace Hopper'
+'Grace Hopper'.toLowerCase();
+console.log(`My name is ${myName}. I am ${myAgeInDogYears} years old in dog years.`);
+/looks like .toLowerCase() does not work?/
