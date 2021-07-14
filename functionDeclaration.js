95486f67eb789b24b716ba203d930a38f9efe6b1
iadod <84059426+iadod@users.noreply.github.com>
2021-07-14 08:33:20 -0600

    Update functionDeclaration.js

--- functionDeclaration.js
+++ functionDeclaration.js
@@ -6,3 +6,8 @@ function greetInSpanish() {
   console.log('Buenas Tardes')
 };
 
+
+function sayThanks(name) {
+  console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+}
+sayThanks('Cole');
